0c4a659f53967966561e81253c20278ed29874f3
Bryan Lim <bryanjlim@outlook.com>
2017-07-18 20:49:11 -0700

    Changed scale logic and renamed variables

--- Widgets/Animation/VarmintWidgetAnimationCollection.cs
+++ Widgets/Animation/VarmintWidgetAnimationCollection.cs
@@ -32,22 +32,22 @@ namespace MonoVarmint.Widgets
 
         //--------------------------------------------------------------------------------------
         /// <summary>
-        /// RotateConsistent - animate a rotation of a specified amount with a consistent speed
+        /// RotateLinear - animate a rotation of a specified amount with a consistent speed
         /// </summary>
         //--------------------------------------------------------------------------------------
         public static VarmintWidgetAnimation RotateLinear(
             double durationSeconds,
-            float rotationAmount)
+            float rotationDegrees)
         {
             return new VarmintWidgetAnimation(durationSeconds, (widget, delta) =>
             {
-                widget.Rotate = rotationAmount * (float)delta;
+                widget.Rotate = rotationDegrees * (float)delta;
             });
         }
 
         //--------------------------------------------------------------------------------------
         /// <summary>
-        /// ScaleConsistent - animate a scale with a specified factor in relation to the current size with a consistent speed
+        /// ScaleLinear - animate a scale with a specified factor in relation to the current size with a consistent speed
         /// </summary>
         //--------------------------------------------------------------------------------------
         public static VarmintWidgetAnimation ScaleLinear(
@@ -57,15 +57,11 @@ namespace MonoVarmint.Widgets
         {
             return new VarmintWidgetAnimation(durationSeconds, (widget, delta) =>
             {
-
-                // Animations scales from 1 (current size) to scale factor
-                float changeFactor = 0;
                 if (scaleFactor < 0) throw new ArgumentException();
-                if (scaleFactor < 1) changeFactor = -scaleFactor;
-                if (scaleFactor >= 1) changeFactor = 1 - scaleFactor;
 
-                widget.Size = originalSize * (1 + (changeFactor * (float)delta));
-
+                // Scales from 1 (current size) to scale factor
+                var sizeChange = originalSize * scaleFactor - originalSize; 
+                widget.Size = originalSize + sizeChange * (float)delta;
             });
         }
 
